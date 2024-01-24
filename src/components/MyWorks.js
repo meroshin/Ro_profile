996a353e4bef17b4abd4689b909e5304a44c67e8
meroshin <me2roshinramesh@gmail.com>
2024-01-24 13:28:36 +0530

    lcommit

--- src/components/MyWorks.js
+++ src/components/MyWorks.js
@@ -67,19 +67,6 @@ function MyWorks() {
 
       </Row>
 
-      <Row style={{padding:"20px 0"}}>
-      <Col xs={2} md={6}> <Card>
-            <Card.Img variant="top"  src={require('../images/portfolio-img.png')} height="300px" />
-            <Card.Body>
-              <Card.Title style={{fontWeight:"600", paddingBottom:"5px"}}>React JS - Website</Card.Title>
-              {/* <Card.Text style={{fontSize:".9rem"}}>
-             <p> Encompassing photography, videography, filmmaking , art and social media.
-              <Link to="https://www.weddzee.com/" target="_blank"> https://www.weddzee.com/</Link></p>
-              </Card.Text> */}
-            </Card.Body>
-          </Card>
-          </Col>
-      </Row>
  
     </Container>
     </div>
